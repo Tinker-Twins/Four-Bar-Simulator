cc7d1a35fcdcdb6736b1416f33b765c84fe33f76
Tinker Twins <tinkertwinsofficial@gmail.com>
2023-01-03 12:45:19 -0500

    Initial commit

---
+++ Assets/Scripts/FourBarController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FourBarController : MonoBehaviour
+{
+    public Rigidbody RigidBody;
+    public float DriveForce = 100;
+
+    private void FixedUpdate()
+    {
+        var worldPoint = this.transform.TransformPoint(new Vector3(.45f, 0, 0));
+        var worldForce = this.transform.TransformVector(new Vector3(0, DriveForce, 0));
+        RigidBody.AddForceAtPosition(RigidBody.mass * worldForce, worldPoint);
+    }
+}
